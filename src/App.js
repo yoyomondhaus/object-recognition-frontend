03b4936a001432dfe4b85c397f2c5da150bb1519
mondhaus <xiaoheizhaiblack@gmail.com>
2023-04-13 23:43:08 +1200

    feature: modify code

--- src/App.js
+++ src/App.js
@@ -1,20 +1,17 @@
-import logo from './logo.svg';
 import HomePage from './pages/HomePage';
 import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
 import FlashcardsPage from './pages/FlashcardsPage';
-
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-
 
 function App() {
     return (
         <Router>
             <Routes>
-                <Route path="/" exact component={HomePage} />
-                <Route path="/login" component={LoginPage} />
-                <Route path="/register" component={RegisterPage} />
-                <Route path="/flashcards" component={FlashcardsPage} />
+                <Route path="/" element={<HomePage />} />
+                <Route path="/login" element={<LoginPage />} />
+                <Route path="/register" element={<RegisterPage />} />
+                <Route path="/flashcards" element={<FlashcardsPage />} />
             </Routes>
         </Router>
     );
