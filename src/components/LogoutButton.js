fe5ed2b03264f2305b87a0b46587e873af079bbd
mondhaus <xiaoheizhaiblack@gmail.com>
2023-04-21 10:27:08 +1200

    feature: Add LoginButton and LogoutButton to HomePage

--- src/components/LogoutButton.js
+++ src/components/LogoutButton.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
 
-function LogoutButton() {
+const LogoutButton = () => {
     const { logout } = useAuth0();
 
-    return <button onClick={() => logout()}>Log Out</button>;
-}
+    return (
+        <button onClick={() => logout({ returnTo: window.location.origin })}>
+            Log Out
+        </button>
+    );
+};
 
 export default LogoutButton;
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,7 +1,17 @@
 import React from "react";
+import {useAuth0} from "@auth0/auth0-react";
+import LoginButton from "../components/LoginButton";
+import LogoutButton from "../components/LogoutButton";
 
 function HomePage() {
-    return <></>
+    const { isAuthenticated } = useAuth0();
+
+    return (
+        <div>
+            <h1>Home Page</h1>
+            {isAuthenticated ? <LogoutButton /> : <LoginButton />}
+        </div>
+    );
 }
 
 export default HomePage;
