7c47cdfd05e27bea0a23bcd614db9b1e52305f14
mondhaus <xiaoheizhaiblack@gmail.com>
2023-04-21 11:00:51 +1200

    feature: Delete LoginPage and RegisterPage

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
 import HomePage from './pages/HomePage';
-import LoginPage from './pages/LoginPage';
-import RegisterPage from './pages/RegisterPage';
 import FlashcardsPage from './pages/FlashcardsPage';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
@@ -9,8 +7,6 @@ function App() {
         <Router>
             <Routes>
                 <Route path="/" element={<HomePage />} />
-                <Route path="/login" element={<LoginPage />} />
-                <Route path="/register" element={<RegisterPage />} />
                 <Route path="/flashcards" element={<FlashcardsPage />} />
             </Routes>
         </Router>
--- src/pages/RegisterPage.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function RegisterPage() {
-    return <></>
-}
-
-export default RegisterPage;
