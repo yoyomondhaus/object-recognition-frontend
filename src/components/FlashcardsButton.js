80428120ee9d71fe886c162ff0a8c498a0292134
mondhaus <xiaoheizhaiblack@gmail.com>
2023-04-21 21:06:49 +1200

    feature: add FlashcardsButton

---
+++ src/components/FlashcardsButton.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const FlashcardsButton = () => {
+    return (
+        <button>
+           Flashcards
+        </button>
+    );
+};
+
+export default FlashcardsButton;
